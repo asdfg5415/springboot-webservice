3bbdf881b5778e674fac886bdf009229206c485d
asdfg5415 <asdfg5415@gmail.com>
2021-02-22 03:26:02 +0900

    챕터 4까지 완료

--- src/main/java/com/ex/brandnew/service/PostsService.java
+++ src/main/java/com/ex/brandnew/service/PostsService.java
@@ -2,13 +2,16 @@ package com.ex.brandnew.service;
 
 import com.ex.brandnew.domain.posts.Posts;
 import com.ex.brandnew.domain.posts.PostsRepository;
+import com.ex.brandnew.web.dto.PostsListResponseDto;
 import com.ex.brandnew.web.dto.PostsResponseDto;
 import com.ex.brandnew.web.dto.PostsSaveRequestDto;
 import com.ex.brandnew.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,8 +36,24 @@ public class PostsService {
                 .orElseThrow(
                         () -> new IllegalArgumentException("잘못된 id 입니다.")
                 );
-        entity.update(requestDto.getTitle(), requestDto.getContent(), requestDto.getAuthor());
+        entity.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("잘못된 id 입니다.")
+                );
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/ex/brandnew/web/PostsApiController.java
+++ src/main/java/com/ex/brandnew/web/PostsApiController.java
@@ -6,6 +6,7 @@ import com.ex.brandnew.web.dto.PostsResponseDto;
 import com.ex.brandnew.web.dto.PostsSaveRequestDto;
 import com.ex.brandnew.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -27,5 +28,11 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
